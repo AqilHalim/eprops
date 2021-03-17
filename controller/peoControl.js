0b165517aafd1d12d56b935e36cac3eb528ef984
Aqil Halim <aqilhalim@yahoo.co.id>
2021-03-17 11:49:33 +0700

    version 3.1, peoControl. Role 'Anak' by default

--- controller/peoControl.js
+++ controller/peoControl.js
@@ -116,8 +116,8 @@ exports.postOne = async function (req, res) {
         const people = await People.create(req.body)
         const id = people.id_people //mengambil id_people dari record people dan memasukkannya dalam variable
         var role = 3
-        if (req.body.status == 'Menikah') {
-            var role = req.body.role
+        if (req.body.status === 'Menikah') {
+            role = req.body.role
         }
         await Family.create({
             id_people: id,
